7f9d52304390cca148cd5917e223bee1070f8d53
Muhammed Mijwad <mijuzz007@gmail.com>
2024-08-03 13:35:47 +0530

    adding to cart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -45,7 +45,8 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart"
+          data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>`;
@@ -55,5 +56,28 @@ products.forEach((product) => {
 const productDiv = document.querySelector('.products-grid');
 
 productDiv.innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+  button.addEventListener('click', () => {
+    const productId = button.dataset.productId;
+    let matchingItem
+
+    cart.forEach((item) => {
+      if (item.productId === productId) {
+        matchingItem = item;
+      }
+    })
+
+    if (matchingItem){
+      matchingItem.quantity += 1
+    } else {
+        cart.push({
+            productId: productId,
+            quantity: 1
+        })
+    }
+    console.log(cart);
+  })
+});
 
 
