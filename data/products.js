f30e75b1cdb6cd3f61c8cf7391c90a5066daa5df
Muhammed Mijwad <mijuzz007@gmail.com>
2024-08-06 12:53:45 +0530

    Inheritance

--- data/products.js
+++ data/products.js
@@ -24,8 +24,26 @@ class Product {
     this.priceCents = productDetails.priceCents;
     this.keywords = productDetails.keywords;
   }
-  getStarsUrl(){
-    return `images/ratings/rating-${this.rating.stars * 10}.png`
+  getStarsUrl() {
+    return `images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+
+  extraInfoHTML(){
+    return '';
+  }
+}
+
+class Clothing extends Product {
+  sizeChartLink;
+  constructor(productDetails) {
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML(){
+    return `
+    <a href=${this.sizeChartLink} target="_blank">Size chart</a>
+    `
   }
 }
 
@@ -501,6 +519,9 @@ export const products = [
     keywords: ["sweaters", "hoodies", "apparel", "mens"],
   },
 ].map((productDetails) => {
+  if(productDetails.type === 'clothing'){
+    return new Clothing(productDetails);
+  }
   return new Product(productDetails);
 });
 
